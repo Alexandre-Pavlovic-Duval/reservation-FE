5ad4c9a01685d4b5ba0326ad79a2e478b8bcce03
Ethan <ethan.frappie@gmail.com>
2025-04-15 12:34:01 +0200

    cart page

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('load').addEventListener('click', async () => {
-    const res = await fetch('http://localhost:5000/api/example');
-    const data = await res.json();
-    const output = document.getElementById('output');
-    output.innerHTML = data.map(item => `<p>${item.title}: ${item.description}</p>`).join('');
-});
