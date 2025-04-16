e7d0cad5c029f583445c42061e1b82679b011079
Ethan <ethan.frappie@gmail.com>
2025-04-16 11:17:17 +0200

    script debug

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
-document.querySelector('.btnSearch').addEventListener("click", function() {
-    let departure = document.querySelector('.departure').value.toLowerCase();
-    let arrival = document.querySelector('.arrival').value.toLowerCase();
-    let rawDate =  document.querySelector('.dateTrip').value.toLowerCase();
-    rawDate = moment(rawDate).format('YYYY-MM-DD');
+document.querySelector(".btnSearch").addEventListener("click", function () {
+  let departure = document.querySelector(".departure").value.toLowerCase();
+  let arrival = document.querySelector(".arrival").value.toLowerCase();
+  let rawDate = document.querySelector(".dateTrip").value.toLowerCase();
+  rawDate = moment(rawDate).format("YYYY-MM-DD");
 
-    // TBD
-    if (document.querySelector(".tripContainer") !== null){
-        console.log("test");
-        const dataToDelete = document.querySelector('tripContainer');
-        dataToDelete.innerHTML = '';
-    }  
+  if (document.querySelector(".tripContainer") !== null){
+    const dataToDelete = document.querySelector('.tripContainer');
+    dataToDelete.innerHTML = '';
+    };
 
-    
-    fetch('http://localhost:3000/trips')
-    .then(response => response.json())
-    .then(data => {
+  fetch("http://localhost:3000/trips")
+    .then((response) => response.json())
+    .then((data) => {
+        let found = false
+      for (let i = 0; i < data.trips.length; i++) {
+        let newDate = data.trips[i].date;
+        let newDateFormated = moment(newDate).format("YYYY-MM-DD");
 
-        for (let i = 0; i < data.trips.length; i++){
-            let newDate = data.trips[i].date;
-            let newDateFormated = moment(newDate).format('YYYY-MM-DD');
-
-            if (data.trips[i].departure.toLowerCase() === departure && data.trips[i].arrival.toLowerCase() === arrival && newDateFormated === rawDate){
-                newDateFormated = moment(newDate).format('HH:mm');
-                document.querySelector(".tripContainer").innerHTML += `
+        if (
+          data.trips[i].departure.toLowerCase() === departure &&
+          data.trips[i].arrival.toLowerCase() === arrival &&
+          newDateFormated === rawDate
+        ) {
+            found = true
+          newDateFormated = moment(newDate).format("HH:mm");
+          document.querySelector(".tripContainer").innerHTML += `
                     <div class="trip">
                         <p class="trips">${departure} > ${arrival}</p>
                         <p class="hours">${newDateFormated}</p>
@@ -30,14 +32,20 @@ document.querySelector('.btnSearch').addEventListener("click", function() {
                         <div class="bookATrip">
                             <button type="button">Book</button>
                         </div>
-                    </div>
-                `
-
-            }
+                    </div>`;
         }
-    })
-    document.querySelector('.departure').value = "";
-    document.querySelector('.arrival').value = "";
-    document.querySelector('.dateTrip').value = "";
+      }
 
+      if(!found){
+        document.querySelector(".tripContainer").innerHTML += `
+        <div class="noTrip">
+            <img src="./images/notfound.png" class="image">
+            <p>It's time to book your future trip</p>
+          </div>` 
+      }
+
+    });
+  document.querySelector(".departure").value = "";
+  document.querySelector(".arrival").value = "";
+  document.querySelector(".dateTrip").value = "";
 });
