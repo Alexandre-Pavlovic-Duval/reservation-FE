81c1a0ce794a275d2571b3875f2ea1a136db42bb
Alexandre Pavlovic-Duval <alexandre.pavlovic.duval@gmail.com>
2025-04-16 17:09:09 +0200

    Correcting the syntax in the cart.js

--- cart.js
+++ cart.js
@@ -69,8 +69,6 @@ document.querySelector('#achat').addEventListener("click", function () {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ trips, hours, price }),
-  })
-
-  console.log("SDDDEZDEZEZD", trips);
+  });
 });
   
